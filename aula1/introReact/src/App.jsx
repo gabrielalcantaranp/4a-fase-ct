9d7619a74b47cce3f4efb6fc26db68cf67d2605d
GABRIEL ALCANTARA NORONHA PINTO <gabriel_an_pinto@estudante.sesisenai.org.br>
2024-07-30 17:12:36 -0300

    Criação do projeto base em React

---
+++ aula1/introReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Conteudo from './components/Conteudo'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Conteudo/>
+    </>
+  )
+}
+
+export default App
---
+++ aula1/introReact/src/components/Conteudo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Conteudo.css'
+
+function Conteudo() {
+  return (
+    <div className='conteudo-container'>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ipsam ipsum iusto repellat et doloribus enim laboriosam reiciendis doloremque laborum dignissimos sint nam rem, ipsa tempore voluptas neque fuga natus.</p>
+    </div>
+  )
+}
+
+export default Conteudo
