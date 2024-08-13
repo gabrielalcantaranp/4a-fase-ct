6877faf942437a754bd7602f87f3619abeab53e5
GABRIEL ALCANTARA NORONHA PINTO <gabriel_an_pinto@estudante.sesisenai.org.br>
2024-08-13 17:14:28 -0300

    Atividade Landing Page de uma Farmácia

---
+++ projetoFarmacia/src/components/Navbar/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Index.css'
+
+function Navbar() {
+  return (
+    <div className='navbar-container'>
+        <div className="divLogo">
+        <img src='./images/logop.png' className='logop-css'></img>
+        </div>
+
+        <div className="divButton">
+        <button className="button">Quero ir!</button>
+        </div>
+        
+      
+   </div>
+  )
+}
+
+export default Navbar
