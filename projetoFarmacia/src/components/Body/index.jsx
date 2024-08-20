f15f6f2555dbf638ef58cd125a92f45063f8be25
GABRIEL ALCANTARA NORONHA PINTO <gabriel_an_pinto@estudante.sesisenai.org.br>
2024-08-20 17:21:26 -0300

    Ultima atualização da atividade Farmácia

--- projetoFarmacia/src/components/Body/index.jsx
+++ projetoFarmacia/src/components/Body/index.jsx
@@ -8,16 +8,30 @@ function Body() {
 
             <div className='body-info'>
 
-                <div className='body-estrelas'>
-                 <Estrelas/>
-                 <h1 className=''>Mais de 150mil Clientes satisfeitos!</h1>
-                
+                <div className='body-top'>
+                    <div className='body-estrelas'>
+                     <Estrelas/>
+                    </div>
+
+                    <div className='body-info-estrelas'>
+                    <h1 className='info-estrelas'>Mais de 150mil Clientes satisfeitos!</h1>
+                    </div>
                 </div>
 
+                <div className='body-down'>
+
+                </div>
+
+                <div className='body-contexto'>
+                    <h1 className='info-contexto'>Renauguração da Farmácia mais querida do bairro!</h1>
+                </div>
+
+                <div className='body-contexto-dois'>
+                    <h1 className='info-contexto'>Renauguração da Farmácia mais querida do bairro!</h1>
+                </div>
             </div>
 
             <div className='body-image'>
-
             </div>
 
         </div>
