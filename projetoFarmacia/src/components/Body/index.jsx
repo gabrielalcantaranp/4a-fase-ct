b2836a882f5291d1068869f72d4eacad01952648
GABRIEL ALCANTARA NORONHA PINTO <gabriel_an_pinto@estudante.sesisenai.org.br>
2024-08-21 14:18:42 -0300

    Ultima atualização da Atividade de uma Landing page de Farmácia

--- projetoFarmacia/src/components/Body/index.jsx
+++ projetoFarmacia/src/components/Body/index.jsx
@@ -27,8 +27,36 @@ function Body() {
                 </div>
 
                 <div className='body-contexto-dois'>
-                    <h1 className='info-contexto'>Renauguração da Farmácia mais querida do bairro!</h1>
+                    <h1 className='info-contexto-dois'>Venha conhecer nosso novo cantinho, com um espaço maior e uma grande variedade de novos produtos. Esperamos você!</h1>
                 </div>
+
+                <div className='body-contexto-tres'>
+                    <div className='linha-um'>
+                        <div className='check-box-um'>
+
+                        </div>
+                    </div>     
+
+                    <div className='linha-dois'>
+                        <div className='check-box-dois'>
+
+                        </div>
+                    </div>  
+
+                    <div className='linha-tres'>
+                        <div className='check-box-tres'>
+
+                        </div>
+                    </div>   
+
+                    <div className='linha-quatro'>
+                        <div className='check-box-quatro'>
+
+                        </div>
+                    </div>                                                            
+                                  
+                </div>
+
             </div>
 
             <div className='body-image'>
