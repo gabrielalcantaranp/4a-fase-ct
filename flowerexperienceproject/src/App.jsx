42ab56ab306631007ac5c071d0443b90c4ffdb9c
GABRIEL ALCANTARA NORONHA PINTO <gabriel_an_pinto@estudante.sesisenai.org.br>
2024-09-25 17:26:03 -0300

    Atualização 25/09 - Situação de Aprendizado

--- flowerexperienceproject/src/App.jsx
+++ flowerexperienceproject/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Navbar from "./components/Navbar/index"
+import Body from "./components/Body/index"
 
 function App() {
 
   return (
     <>
         <Navbar />
+        <Body />
     </>
   )
 }
