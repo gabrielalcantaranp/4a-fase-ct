e3ab4ccf9ea0575526d448f81c53c5413929ad3d
GABRIEL ALCANTARA NORONHA PINTO <gabriel_an_pinto@estudante.sesisenai.org.br>
2024-09-25 14:16:41 -0300

    Atualização Situação de Aprendizado

--- flowerexperienceproject/src/components/Navinfo/index.jsx
+++ flowerexperienceproject/src/components/Navinfo/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { FaUser } from "react-icons/fa";
-import { FaStar } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
 import './index.css'
 
 function index() {
     return (
         <div>
-            <button className='buttonInfos'><FaSearch className='icon-css'/></button>
-            <button className='buttonInfos'><FaUser className='icon-css'/></button>
-            <button className='buttonInfos'><FaStar className='icon-css'/></button>
+            <button className='buttonInfos'><IoSearch className='icon-css'/></button>
+            <button className='buttonInfos'><FaRegUser className='icon-css'/></button>
+            <button className='buttonInfos'><FaRegStar className='icon-css'/></button>
         </div>
     )
 
